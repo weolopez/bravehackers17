9ddcd47866a5faa01748c5ee2330d64eff27cc5c
Lopez, Mauricio (ml5174) <ml5174@att.com>
2017-03-29 23:49:35 -0400

    added tinymce editor

---
+++ src/pages/calendar/calendar.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+declare var tinymce: any;
+
+@Component({
+  selector: 'page-calendar',
+  templateUrl: 'calendar.html'
+})
+export class CalendarPage {
+
+  constructor(){}
+}
